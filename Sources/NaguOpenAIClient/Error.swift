7232f56880184920b93484a5b8442cd2a7a08f0a
NavaneethN-N <navaneethn@success4.us>
2024-01-27 17:11:41 +0530

    updated error

--- Sources/NaguOpenAIClient/Error.swift
+++ Sources/NaguOpenAIClient/Error.swift
@@ -1,13 +1,6 @@
-//
-//  Error.swift
-//
-//
-//  Created by Alfian Losari on 08/11/23.
-//
-
 import Foundation
 
 extension String: LocalizedError {
-    
+
     public var errorDescription: String? { self }
 }
